8bf1bebd030ad9a3e55c91ce1d7230b509ce5cfe
Marcus Ruohonen <marcus.ruohonen@gmail.com>
2024-02-12 10:33:29 +0200

    viime muutokset

--- kysely/views.py
+++ kysely/views.py
@@ -1,25 +1,49 @@
-from django.http import Http404, HttpResponse
+from django.urls import reverse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.views import generic
 
-from .models import Kysymys
-
-
-def indeksi(request):
-    kysymyslista = Kysymys.objects.order_by("-julkaisupvm")[:2]
-    context = {
-        "kysymykset": kysymyslista,
-    }
-    return render(request, "kysely/indeksi.html", context)
+from .models import Kysymys, Vaihtoehto
 
 
-def näytä(request, kysymys_id):
+class ListaNäkymä(generic.ListView):
+    template_name = "kysely/indeksi.html"
+    context_object_name = "kysymykset"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Kysymys.objects.order_by("-julkaisupvm")[:2]
+
+class NäytäNäkymä(generic.DetailView):
+    model = Kysymys
+    template_name = "kysely/näytä.html"
+
+
+class TuloksetNäkymä(generic.DetailView):
+    model = Kysymys
+    template_name = "kysely/tulokset.html"
+
+
+def äänestä(request, kysymys_id):
     kysym = get_object_or_404(Kysymys, pk=kysymys_id)
-    return render(request, "kysely/näytä.html", {"kysymys": kysym})
-
-
-def tulokset(request, question_id):
-    return HttpResponse(f"Katsot kysymyksen {question_id} tuloksia")
-
-
-def äänestä(request, question_id):
-    return HttpResponse(f"Olet äänestämässä kysymykseen {question_id}")
+    try:
+        valittu = kysym.vaihtoehto_set.get(pk=request.POST["valittu"])
+    except (KeyError, Vaihtoehto.DoesNotExist):
+        # Näytä kysymyslomake uudelleen.
+        return render(
+            request,
+            "kysely/näytä.html",
+            {
+                "kysymys": kysym,
+                "virheviesti": "Et valinnut mitään vaihtoehtoa.",
+            },
+        )
+    else:
+        valittu.ääniä = valittu.ääniä + 1
+        valittu.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        osoite = reverse("kysely:tulokset", args=(kysym.id,))
+        return HttpResponseRedirect(osoite)
+    
